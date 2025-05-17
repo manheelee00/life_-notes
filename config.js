f4bb006f8a4f941f54beb3ff5e030efa6662cc03
manhee lee <168778682+manheelee00@users.noreply.github.com>
2025-05-17 18:27:00 +0900

    사이트 설정 및 작성자 정보 업데이트

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // 기본 설정 파일
 // 여기서 사용자 입장에서 꼭 입력해야 하는 정보를 의사결정해서 개발에 반영할 필요가 있습니다.
 const siteConfig = {
-  username: "", // GitHub 사용자 이름
-  repositoryName: "", // GitHub 저장소 이름
-  mainColor: "#3498db", // 사이트의 주 색상
-  textColor: "#333333", // 기본 텍스트 색상
-  blogTitle: "", // 블로그 제목
+  username: "manheelee00", // GitHub 사용자 이름
+  repositoryName: "life_-notes", // GitHub 저장소 이름
+  mainColor: "#222222", // 사이트의 주 색상
+  textColor: "#f0f0f0", // 기본 텍스트 색상
+  blogTitle: "라이프 노트", // 블로그 제목
 };
 
 // 여러명의 저자가 글을 쓸 경우 프로필 설정, default는 0번째 사용자
